f228a86d6aead2beea28b4b4f9455c2b07249ff1
Wanderson dos santos <wandersonneed@hotmail.com>
2013-11-20 14:38:33 -0200

    Update trabalho-nayane.cpp

--- trabalho-nayane.cpp
+++ trabalho-nayane.cpp
@@ -3,3 +3,13 @@
 #include<iostream>
 #include<cstdlib>
 using namespace std;
+
+
+// Estrutura de cadastro do aluno
+struct Cadastro
+{
+          string matricula;
+          string nome;
+          string disciplina;
+          double nota;
+};
